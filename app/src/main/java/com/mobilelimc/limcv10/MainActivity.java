2115abf64f2acf118d6c7a1a2e36e2713ef710ca
llbaramll <llbaramll@gmail.com>
2026-07-06 20:41:17 +0900

    Create MainActivity.java

---
+++ app/src/main/java/com/mobilelimc/limcv10/MainActivity.java
@@ -0,0 +1,40 @@
+package com.mobilelimc.limcv10;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView myWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+
+        myWebView = new WebView(this);
+        setContentView(myWebView);
+
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+
+        myWebView.setWebViewClient(new WebViewClient());
+        myWebView.loadUrl("https://prod.limc.co.kr/m/login.jsp");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (myWebView.canGoBack()) {
+            myWebView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
