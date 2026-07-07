81c84336a02a1ee33dfc0fc81427d4150af63f3f
llbaramll <llbaramll@gmail.com>
2026-07-07 11:08:52 +0900

    Update MainActivity.java

--- app/src/main/java/com/mobilelimc/limcv10/MainActivity.java
+++ app/src/main/java/com/mobilelimc/limcv10/MainActivity.java
@@ -1,6 +1,7 @@
 package com.mobilelimc.limcv10;
 
 import android.os.Bundle;
+import android.webkit.CookieManager;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -24,8 +25,21 @@ public class MainActivity extends AppCompatActivity {
         WebSettings webSettings = myWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
         webSettings.setDomStorageEnabled(true);
+        
+        // [필수 설정 1] 쿠키 및 세션 유지를 위한 안드로이드 시스템 권한 활성화
+        CookieManager cookieManager = CookieManager.getInstance();
+        cookieManager.setAcceptCookie(true);
+        cookieManager.setAcceptThirdPartyCookies(myWebView, true); // 서드파티 쿠키 허용 (로그인 유지에 필수)
 
-        myWebView.setWebViewClient(new WebViewClient());
+        myWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                // [필수 설정 2] 페이지가 로딩될 때마다 쿠키/세션을 강제로 동기화(Sync)시킵니다.
+                CookieManager.getInstance().flush();
+            }
+        });
+        
         myWebView.loadUrl("https://prod.limc.co.kr/m/login.jsp");
     }
 
